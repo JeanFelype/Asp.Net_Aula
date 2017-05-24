cd5e5b7c8583b6d3d6357bc02a134aa80d7fe8a6
1614833 <1614833@045LAB04013>
2017-05-24 09:38:57 -0300

    Parte do Projeto de Model Frist

---
+++ Aula2405_EF_MF/Aula2405_EF_MF/Views/BasePage.cs
@@ -0,0 +1,14 @@
+﻿using Aula2405_EF_MF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace Aula2405_EF_MF.Views
+{
+    public class BasePage : Page
+    {
+        protected BaseDadosContainer contexto = new BaseDadosContainer();
+    }
+}
