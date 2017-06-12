16e2b143253ee614383f81a82735834c9537a817
1614833 <1614833@045LAB04013>
2017-06-12 10:48:41 -0300

    Alterações

--- Aula1206/Aula1206/Controllers/CategoriasController.cs
+++ Aula1206/Aula1206/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aula1206.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,17 +12,37 @@ namespace Aula1206.Controllers
         // GET: Categorias
         public ActionResult Index()
         {
-            List<string> categorias = new List<string>();
+           // List<string> categorias = new List<string>();
 
-            categorias.Add("Carros");
-            categorias.Add("Motos");
-            categorias.Add("Barcos");
-            categorias.Add("Aviões");
-            categorias.Add("Caminhões");
+            //categorias.Add("Carros");
+            //categorias.Add("Motos");
+            //categorias.Add("Barcos");
+            //categorias.Add("Aviões");
+            //categorias.Add("Caminhões");
+            List<Categoria> categorias = new List<Categoria>();
+
+            categorias.Add(new Categoria() { Nome = "Carros" , Descricao="Lancer"});
+            categorias.Add(new Categoria() { Nome = "Motos", Descricao = "Pop 100" });
+            categorias.Add(new Categoria() { Nome = "Barcos", Descricao = "Lancha" });
+            categorias.Add(new Categoria() { Nome = "Aviões", Descricao = "Jato" });
+            categorias.Add(new Categoria() { Nome = "Caminhões", Descricao = "Volvo" });
+            
 
             ViewBag.ListaCategorias = categorias;
 
+            return View(categorias);
+        }
+
+        //GET 
+        public ActionResult Create()
+        {
             return View();
+        }
+
+       [HttpPost]
+        public ActionResult Create(Categoria categorias)
+        {
+            return View(categorias);
         }
     }
 }
